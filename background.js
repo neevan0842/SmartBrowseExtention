0ca4e85019c2a82345ebc602b58187c188e5ee43
Naveen Manoj <manojnaveen085@gmail.com>
2025-01-28 23:55:23 +0530

    updated the video list

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 const uniqueLinks = [
-  "https://www.youtube.com/watch?v=2urF6xI06r0",
-  "https://www.youtube.com/watch?v=PIaNkWufmns",
-  "https://www.youtube.com/watch?v=wnHW6o8WMas&t=140s",
-  "https://www.youtube.com/watch?v=_KmsNRHVudk",
-  "https://www.youtube.com/watch?v=AKRI6YiWYHE",
-  "https://www.youtube.com/watch?v=DyW8cPeuEQg",
-  "https://www.youtube.com/watch?v=JUeIGphgmcU",
-  "https://www.youtube.com/watch?v=YC08O_AfKgM&t=14s",
-  "https://www.youtube.com/watch?v=mECfzmFvOQU",
-  "https://www.youtube.com/watch?v=TZm2fpECa8g",
-  "https://www.youtube.com/watch?v=jHnX-nMl59U",
+  "https://www.youtube.com/watch?v=az6NibAUf7Y",
+  "https://www.youtube.com/watch?v=VSceuiPBpxY",
+  "https://www.youtube.com/watch?v=sEmZIi_0Kj8",
+  "https://www.youtube.com/watch?v=ft_DXwgUXB0",
+  "https://www.youtube.com/watch?v=-6XkpBd62P0",
+  "https://www.youtube.com/watch?v=dww3Oo8ropA",
+  "https://www.youtube.com/watch?v=L8UT0iVne24",
+  "https://www.youtube.com/watch?v=dIyrR8forI4",
+  "https://www.youtube.com/watch?v=-ESQmzDbnL8",
+  "https://www.youtube.com/watch?v=FRk4PE5GhoU",
+  "https://www.youtube.com/watch?v=svDiqqnGCeU",
 ];
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
