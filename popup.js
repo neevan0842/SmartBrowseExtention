6f6ef074e91cfc68c15125320521a162a9814e23
Naveen Manoj <manojnaveen085@gmail.com>
2025-03-23 10:55:09 +0530

    added medium paywall bypass

---
+++ popup.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("redirectButton");
+  if (!button) return;
+  button.addEventListener("click", () => {
+    // Get the current active tab
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs.length === 0) return;
+
+      const currentUrl = tabs[0].url;
+
+      // If on a Medium article, redirect to Freedium
+      const freediumURL = `https://freedium.cfd/${currentUrl}`;
+      chrome.tabs.update(tabs[0].id, { url: freediumURL });
+
+      window.close();
+    });
+  });
+});
